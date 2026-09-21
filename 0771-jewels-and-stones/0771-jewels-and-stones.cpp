acb4720244413e6ceac5b74307aebf1fe31bcddb
ayush122211 <ayushoct054@gmail.com>
2026-09-21 16:26:19 +0530

    Time: 0 ms (100%), Space: 8.7 MB (22.72%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
-    int numJewelsInStones(string jewels, string stones) {
-        int ans=0;
+     int numJewelsInStones(string jewels, string stones) {
+    //     int ans=0;
+    //     unordered_map<char,int>mp;
+    //     for(char  ch: jewels){
+    //         mp[ch]++;
+    //     }
+    //     for(auto ch: stones){
+    //         if(mp.find(ch)!=mp.end())
+    //         ans++;
+    //     }
+    //     return ans;
+
+
+          int ans=0;
         unordered_map<char,int>mp;
         for(char  ch: jewels){
-            mp[ch]++;
+            mp[ch] = true;
         }
         for(auto ch: stones){
-            if(mp.find(ch)!=mp.end())
+            if(mp[ch] == true)
             ans++;
         }
         return ans;
+
+
     }
 };
